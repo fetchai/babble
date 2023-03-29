f4f9f31e6b9ba6e33b70cf84fa6ffabae88e783c
James Riehl <james.riehl@fetch.ai>
2023-03-29 17:47:32 +0100

    feat: allow config from env vars

--- src/babble/config.py
+++ src/babble/config.py
@@ -1,3 +1,4 @@
-# AUTH_SERVER = 'https://auth-attila.sandbox-london-b.fetch-ai.com'
-AUTH_SERVER = "http://127.0.0.1:15500"
-MEMORANDUM_SERVER = "http://127.0.0.1:4000"
+import os
+
+AUTH_SERVER = os.environ.get("AUTH_SERVER", "http://127.0.0.1:15500")
+MEMORANDUM_SERVER = os.environ.get("MEMORANDUM_SERVER", "http://127.0.0.1:4000")
