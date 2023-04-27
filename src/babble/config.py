772f08d420a0893799bb4fff73cc1a3e1ce59690
agent-dominatrix <128386379+agent-dominatrix@users.noreply.github.com>
2023-04-27 11:13:45 +0530

    fix: lint issues

--- src/babble/config.py
+++ src/babble/config.py
@@ -1,4 +1,9 @@
 import os
 
-AUTH_SERVER = os.environ.get("AUTH_SERVER", "https://auth-attila.sandbox-london-b.fetch-ai.com")
-MEMORANDUM_SERVER = os.environ.get("MEMORANDUM_SERVER", "https://messaging-server.sandbox-london-b.fetch-ai.com/graphql")
+AUTH_SERVER = os.environ.get(
+    "AUTH_SERVER", "https://auth-attila.sandbox-london-b.fetch-ai.com"
+)
+MEMORANDUM_SERVER = os.environ.get(
+    "MEMORANDUM_SERVER",
+    "https://messaging-server.sandbox-london-b.fetch-ai.com/graphql",
+)
