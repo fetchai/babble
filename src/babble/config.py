eddd1c13af80f33513fdeafe4f6b12b4cdf15883
agent-dominatrix <128386379+agent-dominatrix@users.noreply.github.com>
2023-04-27 13:22:42 +0530

    feat: integrate message id (#11)

--- src/babble/config.py
+++ src/babble/config.py
@@ -1,4 +1,9 @@
 import os
 
-AUTH_SERVER = os.environ.get("AUTH_SERVER", "http://127.0.0.1:15500")
-MEMORANDUM_SERVER = os.environ.get("MEMORANDUM_SERVER", "http://127.0.0.1:4000")
+AUTH_SERVER = os.environ.get(
+    "AUTH_SERVER", "https://auth-attila.sandbox-london-b.fetch-ai.com"
+)
+MEMORANDUM_SERVER = os.environ.get(
+    "MEMORANDUM_SERVER",
+    "https://messaging-server.sandbox-london-b.fetch-ai.com/graphql",
+)
