a88dc615b86c7ab90ad897f392dbd41e86f5d27c
James Riehl <33920192+jrriehl@users.noreply.github.com>
2024-03-06 13:15:29 +0000

    chore: update default memorandum server (#17)

--- src/babble/config.py
+++ src/babble/config.py
@@ -3,5 +3,5 @@ import os
 AUTH_SERVER = os.environ.get("AUTH_SERVER", "https://accounts.fetch.ai/v1")
 MEMORANDUM_SERVER = os.environ.get(
     "MEMORANDUM_SERVER",
-    "https://messaging-server.sandbox-london-b.fetch-ai.com/graphql",
+    "https://messaging.fetch-ai.network",
 )
